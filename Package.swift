cf43f4e6346c4bc96ee2905f218bf91aa9f9fca1
Alex Hüllmandel <alx91@me.com>
2018-10-04 07:55:06 +0200

    Update Package.swift
    Use 1.1.* version from aws swift core

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
       .library(name: "SwiftAWSEmail", targets: ["SwiftAWSEmail"]),
   ],
   dependencies: [
-      .package(url: "https://github.com/noppoMan/aws-sdk-swift-core.git", .upToNextMajor(from: "1.0.0"))
+      .package(url: "https://github.com/noppoMan/aws-sdk-swift-core.git", .upToNextMajor(from: "1.1.0"))
   ],
   targets: [
       .target(name: "SwiftAWSEmail", dependencies: ["AWSSDKSwiftCore"]),
